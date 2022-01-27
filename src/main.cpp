b3bc8e5c2a61e2bb4d6fd0c04a9c993b0e8f30ba
Thomas <drivemenuts766@gmail.com>
2022-01-26 19:10:11 -0500

    added support for 4x4 matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,35 @@
 #include <Arduino.h>
 
 /*keypad matrix dimensions*/
-#define matrixSize 2
+#define matrixSize 4
 
-/*wait 20 ms before accepting a repeated input*/
-#define timerCount 300
-/*Wait 1s before accepting a duplicate input*/
+/*wait 0.1 ms before accepting a input*/
+#define timerCount 100
+/*Wait 1.2s before accepting a duplicate input*/
 #define repeatCount 12000
 
+/*wait 150ms between subsequent repeated inputs after the second one*/
 #define repeatShortCount 1500
 
 /*GPIO pins used for rows & columns*/
 #define C1 12
 #define C2 13
+#define C3 14
+#define C4 15
 
 #define R1 16
 #define R2 17
+#define R3 18
+#define R4 19
 
 /*column that is pulled high*/
-volatile uint8_t activeColumn;
+volatile uint8_t activeColumn  = 0;
 /*value of button to be consumed in loop()*/
-volatile uint8_t btnPressed;
+volatile uint8_t btnPressed = 0;
 /*most recently captured input*/
-uint8_t lastPressed;
+volatile uint8_t lastPressed = 0;
 /*row that is pulled high*/
-volatile int8_t activeRow;
+volatile int8_t activeRow = -1;
 /*timer to wait a delay to debounce button sufficiently*/
 hw_timer_t * timer = NULL;
 /*timer to wait a delay before accepting repeat inputs*/
@@ -32,15 +37,30 @@ hw_timer_t * repeatTimer = NULL;
 
 /*keypad matrix is as follows*/
 //uint8_t btnMatrix[matrixSize][matrixSize]= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-uint8_t btnMatrix[matrixSize][matrixSize]= {{1,2},{3,4}};
+uint8_t btnMatrix[matrixSize][matrixSize]= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 /*
       | 1 |  | 2 |  | 3 |  | 4 | 
       | 5 |  | 6 |  | 7 |  | 8 | 
      | 9 |  | 10 |  | 11 |  | 12 | 
      | 13 |  | 14 |  | 15 |  | 16 | 
 */
+
 portMUX_TYPE interruptMux = portMUX_INITIALIZER_UNLOCKED;
 
+void setRepeatTimer(bool setShort)
+{
+  timerAlarmDisable(repeatTimer);
+  timerAlarmWrite(repeatTimer, setShort ? repeatShortCount : repeatCount, false);
+}
+
+void startTimer(hw_timer_t * tim)
+{
+  timerRestart(tim);
+  timerAlarmEnable(tim);
+}
+
+
+/*Convert row pin number into 0 based index*/
 uint8_t cheekyIndex(uint8_t row)
 {
   return row -16;
@@ -56,14 +76,10 @@ uint8_t checkPressed(uint8_t row)
   {
     /*Set row to active and start timer to repeat until the button is pulled low*/
     activeRow = row;
-    timerAlarmEnable(timer);
+    startTimer(timer);
   }
   return result;
 }
-
-
-
-
 
 void IRAM_ATTR pressedTimer() {
   portENTER_CRITICAL_ISR(&interruptMux);
@@ -78,21 +94,17 @@ void IRAM_ATTR pressedTimer() {
       btnPressed = btnMatrix[cheekyIndex(activeRow)][activeColumn];
       lastPressed = btnPressed;
       /*Start repeat timer to accept repeated input after delay*/
-      timerWrite(repeatTimer, 0);
-      timerAlarmEnable(repeatTimer);
+      startTimer(repeatTimer);
     }
   }
   /*after delay, button became low so start cycling again & stop timers*/
   else
   {
+    timerAlarmDisable(timer);
+    setRepeatTimer(false);
     Serial.println("Button Released");
     activeRow = -1;
     lastPressed = 0;
-    timerAlarmDisable(timer);
-    timerWrite(timer, 0);
-    timerAlarmDisable(repeatTimer);
-    timerAlarmWrite(repeatTimer, repeatCount, false);
-    timerWrite(repeatTimer, 0);
   }
   interrupts();
  portEXIT_CRITICAL_ISR(&interruptMux);
@@ -102,23 +114,14 @@ void IRAM_ATTR clearRepeat() {
   portENTER_CRITICAL_ISR(&interruptMux);
   noInterrupts();
   /*After delay, clear value*/
+  setRepeatTimer(true);
   lastPressed = 0;
-  timerAlarmDisable(repeatTimer);
-  timerAlarmWrite(repeatTimer, repeatShortCount, false);
-  
   interrupts();
  portEXIT_CRITICAL_ISR(&interruptMux);
 }
 
 
 void setup() {
-
-  /*Init variables*/
-  activeColumn = 0;
-  activeRow = -1;
-  btnPressed = 0;
-  lastPressed = 0;
-
   /*Init internals*/
   Serial.begin(115200);
   timer = timerBegin(1, 8000, true);
@@ -126,15 +129,18 @@ void setup() {
   timerAttachInterrupt(timer,pressedTimer,true);
   timerAttachInterrupt(repeatTimer,clearRepeat,true);
   timerAlarmWrite(timer, timerCount, true);
-  timerAlarmWrite(repeatTimer, repeatCount, false);
+  setRepeatTimer(false);
 
   /*Init IO*/
   pinMode(C1,OUTPUT);
   pinMode(C2,OUTPUT);
+  pinMode(C3,OUTPUT);
+  pinMode(C4,OUTPUT);
 
   pinMode(R1,INPUT);
   pinMode(R2,INPUT);
-
+  pinMode(R3,INPUT);
+  pinMode(R4,INPUT);
 }
 
 void loop() {
@@ -142,15 +148,17 @@ void loop() {
   delay(10);
   portENTER_CRITICAL(&interruptMux);
   noInterrupts();
-  /*Cycle keypad when no button is pressed*/
-  if(activeRow == -1)
+  /*Cycle keypad when no button is pressed && most recent input has been consumed*/
+  if(activeRow == -1 && btnPressed ==0)
   {
     /*Cycle columns of keypad*/
     activeColumn = (activeColumn+1) %matrixSize;
     digitalWrite(C1, activeColumn == 0);
     digitalWrite(C2, activeColumn == 1);
+    digitalWrite(C3, activeColumn == 2);
+    digitalWrite(C4, activeColumn == 3);
 
-    if(checkPressed(R1) || checkPressed(R2))
+    if(checkPressed(R1) || checkPressed(R2) || checkPressed(R3)|| checkPressed(R4))
     {
       Serial.println("Button Pressed");
     }
